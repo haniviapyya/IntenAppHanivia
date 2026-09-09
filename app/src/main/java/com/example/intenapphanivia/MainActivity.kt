a7f9ced6a5171cb24a8a78c51dd8f942ae3bf638
AtiqhAbdillah <alatiqhwarithabdillah@mail.ugm.ac.id>
2026-09-09 08:54:21 +0700

    Menambahkan SecondActivity dan memperbarui MainActivity

--- app/src/main/java/com/example/intenapphanivia/MainActivity.kt
+++ app/src/main/java/com/example/intenapphanivia/MainActivity.kt
@@ -1,20 +1,68 @@
 package com.example.intenapphanivia
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.intenapphanivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+private val TAG = "MainActivityLifecycle"
+
+//Buat properti untuk simpan binding
+private lateinit var binding: ActivityMainBinding
+
+companion object{
+    const val EXTRA_NAME = "extra_name"
+}
+
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+
+    //Inisialisasi binding
+    binding = ActivityMainBinding.inflate(layoutInflater)
+
+    //set UI
+    setContentView(binding.root)
+
+    //gunakan binding
+    with(binding) {
+        btnToSecondActivity.setOnClickListener {
+            val intentToSecondActivity =
+                Intent(this@MainActivity, SecondActivity::class.java)
+            startActivity(intentToSecondActivity)
         }
+    }
+
+}
+
+override fun onStart() {
+    super.onStart()
+    Log.d(TAG, "onStart: dipanggil")
+    }
+
+override fun onResume() {
+    super.onResume()
+    Log.d(TAG, "onResume: dipanggil")
+    }
+
+override fun onPause() {
+    super.onPause()
+    Log.d(TAG, "onPause: dipanggil")
+    }
+
+override fun onStop() {
+    super.onStop()
+    Log.d(TAG, "onStop: dipanggil")
+    }
+
+override fun onDestroy() {
+    super.onDestroy()
+    Log.d(TAG, "onDestroy: dipanggil")
     }
 }
---
+++ app/src/main/java/com/example/intenapphanivia/SecondActivity.java
@@ -0,0 +1,4 @@
+package com.example.intenapphanivia;
+
+public class SecondActivity {
+}
